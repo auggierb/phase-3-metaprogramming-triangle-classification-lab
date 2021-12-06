aa9839d6420770bec25232c4c87dda12980e30bd
August <auggierutter@gmail.com>
2021-12-06 13:50:13 -0500

    commit tree 8b4e169a52b96683159b55d57fd1fc386943b0f3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+def  initialize(l1,l2,l3)
+  @l1 = length_1
+  @l2 = length_2
+  @l3 = length_3
+end
+def kind
+  if length_1 && length_2 == length_3
+  :equilateral
+ 
+end
+
+end
+class TriangleError < StandardError
+
+end
 end
